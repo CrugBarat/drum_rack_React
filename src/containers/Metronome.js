15277bcfa0286d928c51e48dce9c65337145f904
CrugBarat <craigtbarratt@gmail.com>
2020-06-17 12:29:33 +0100

    adds sound and click event
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/containers/Metronome.js
+++ src/containers/Metronome.js
@@ -1,20 +1,27 @@
 import React, {Component, Fragment} from 'react';
+import sound from '../assets/audio/sound.wav';
 
 class Metronome extends Component {
   constructor() {
     super();
 
     this.state = {
-      bpm: 140
+      bpm: 90,
+      sound: new Audio(sound)
     }
+
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick() {
+    setInterval(() => this.state.sound.play(), (60000/this.state.bpm));
   }
 
   render() {
     return (
       <Fragment>
         <h1>{this.state.bpm}</h1>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={this.onButtonClick}>Start/Stop</button>
       </Fragment>
     )
   }
